01a428152297906cedab9c474f89467643bc492a
Janine B. Fernandez <janinebfernandez@gmail.com>
2019-02-07 23:13:17 -0800

    Update
    Added Project, Images updated

--- js/project-popup.js
+++ js/project-popup.js
@@ -4,10 +4,16 @@
             $(".trigger_popup_fricc").click(function(){
                $('.hover_bkgr_fricc').show();
             });
+            $(".trigger_popup_fricc_2").click(function(){
+                $('.hover_bkgr_fricc_2').show();
+             });
             //$('.hover_bkgr_fricc').click(function(){
               //  $('.hover_bkgr_fricc').hide();
             //});
             $('.popupCloseButton').click(function(){
                 $('.hover_bkgr_fricc').hide();
+            });
+            $('.popupCloseButton').click(function(){
+                $('.hover_bkgr_fricc_2').hide();
             });
         });
