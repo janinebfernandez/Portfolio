3290e2a7fd4faf597c6f84479ac31729a4fb8ebc
Janine B. Fernandez <janinebfernandez@gmail.com>
2019-08-20 22:48:04 -0700

    Responsive/icon
    icon update and responsive design

--- js/project-popup.js
+++ js/project-popup.js
@@ -1,19 +1,24 @@
 /*PROJECT POP UP*/
 
         $(window).load(function () {
+            /*POP*/
             $(".trigger_popup_fricc").click(function(){
                $('.hover_bkgr_fricc').show();
             });
             $(".trigger_popup_fricc_2").click(function(){
                 $('.hover_bkgr_fricc_2').show();
              });
-            //$('.hover_bkgr_fricc').click(function(){
-              //  $('.hover_bkgr_fricc').hide();
-            //});
+             $(".trigger_popup_fricc_3").click(function(){
+                $('.hover_bkgr_fricc_3').show();
+             });
+            /*CLOSE*/
             $('.popupCloseButton').click(function(){
                 $('.hover_bkgr_fricc').hide();
             });
             $('.popupCloseButton').click(function(){
                 $('.hover_bkgr_fricc_2').hide();
+            });
+            $('.popupCloseButton').click(function(){
+                $('.hover_bkgr_fricc_3').hide();
             });
         });
