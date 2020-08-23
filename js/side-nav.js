9fafa16b3899a992b4dd6d7ff1583590e89fc162
Janine B. Fernandez <janinebfernandez@gmail.com>
2020-08-22 19:40:44 -0700

    Add files via upload

---
+++ js/side-nav.js
@@ -0,0 +1,8 @@
+/*SIDE NAV JS - PORTFOLIO 2021*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
